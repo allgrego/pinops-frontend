6a632350819e25fe48b862bf607856aa6e8824d1
Gregorio Alvarez <allgrego14@gmail.com>
2025-04-16 09:59:39 -0400

    [feature] Routes helper set and login set with backend

--- src/modules/auth/types/auth.d.ts
+++ src/modules/auth/types/auth.d.ts
@@ -1,5 +1,20 @@
-import { UserRoles } from "../setup/auth";
+import { UserRoles } from "@/modules/auth/setup/auth";
 
 export type UserRoleKey = keyof typeof UserRoles;
 
 export type UserRole = (typeof UserRoles)[UserRoleKey];
+
+/**
+ * - - - Login
+ */
+// Backend
+export interface UserLoginBackend {
+  email: string;
+  password: string;
+}
+
+// Internal
+export interface UserLogin {
+  email: UserLoginBackend["email"];
+  password: UserLoginBackend["password"];
+}
